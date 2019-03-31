235f8fa4e07091f15cd17b28c1c18fadf6ce1cd5
jnowaczek <jnowaczek@users.noreply.github.com>
2019-03-31 16:40:13 -0500

    Revert "AVMU crap"
    This reverts commit 7c0bff9e

--- ui/src/app/microwave.service.ts
+++ ui/src/app/microwave.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {WebsocketService} from './websocket.service';
 import {Subject} from 'rxjs';
-import {bufferTime, zip} from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 
 const wsUrl = 'ws://192.168.1.12:/';
 
@@ -17,17 +17,13 @@ export class MicrowaveService {
 
 	constructor(private websocket: WebsocketService) {
 		this.socket = websocket.connect(wsUrl);
-		this.socket.pipe(bufferTime(500), zip((buffer: MessageEvent[]): MovementData => {
-			for (let msg of buffer) {
-				if (msg.data.movement == 1) {
-					return {movement: 1, time: 0};
-				} else {
-					return {movement: 0, time: 0};
-				}
-			}
-		})).subscribe(
-			(response: MovementData) => {
-				this.movementData.next(response);
+		this.socket.subscribe(
+			(response: MessageEvent) => {
+				let data = parseInt(response.data);
+				this.movementData.next({
+					movement: data,
+					time: new Date().getTime()
+				});
 			});
 	}
 }
